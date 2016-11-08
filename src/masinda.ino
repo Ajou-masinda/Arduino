a01925fd8c8764c6adff2c756e58cb0298910071
jungmo <ajm100@ajou.ac.kr>
2016-11-09 02:37:51 +0900

    Work well - MQ2, MQ135, buzzer, serial comm.

---
+++ src/masinda.ino
@@ -0,0 +1,36 @@
+#include "pitches.h"
+
+int mq2_sensorValue; // GAS
+int mq135_sensorValue; // CO2, CO
+int buzzer_pin = 2;
+int mq135_pin =  A0;
+int mq2_pin =   A3;
+int melody = NOTE_G4;
+
+void setup()
+{
+  Serial.begin(9600);      // sets the serial port to 9600
+  
+}
+
+void loop()
+{
+  char msg[11];
+  mq2_sensorValue = analogRead(mq2_pin);      // read analog input pin 0
+  mq135_sensorValue = analogRead(mq135_pin);  
+  sprintf(msg, "%4d, %4d\n", mq2_sensorValue, mq135_sensorValue);
+  Serial.print(msg);
+  /*
+  Serial.print(mq2_sensorValue);
+  Serial.print(",");
+  Serial.print(mq135_sensorValue);  // prints the value read
+  Serial.println();
+  */
+  delay(1000);                        // wait 100ms for next reading
+  
+  if(mq2_sensorValue > 500 || mq135_sensorValue > 800)
+      tone(buzzer_pin, melody, 50);
+  else
+    noTone(buzzer_pin);
+
+}
